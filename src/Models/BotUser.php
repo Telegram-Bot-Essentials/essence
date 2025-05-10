689b800ed4f893547201f55432965b34580f0edc
Elyar <elyareranjbar@gmail.com>
2025-05-10 12:59:21 +0330

    Static keys

--- src/Models/BotUser.php
+++ src/Models/BotUser.php
@@ -4,6 +4,7 @@ namespace Elyar\TelegramBotEssentials\Models;
 
 use Elyar\TelegramBotEssentials\Exceptions\LogicException;
 use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\Admin\AdminPanelKey;
+use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\Admin\BotSettingsKey;
 use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\Member\CancelProcessKey;
 use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\Member\MainMenuKey;
 use Elyar\TelegramBotEssentials\Traits\CanResolveReplyKey;
@@ -48,6 +49,7 @@ class BotUser extends Model
             $rows[] = [AdminPanelKey::class];
         } elseif ($this->menu == 'admin') {
             $rows = array_merge($rows, config('telegram-bot-essentials.keyboard.admin'));
+            $rows[] = [BotSettingsKey::class];
             $rows[] = [MainMenuKey::class];
         }
         return $this->keyboardGenerator($rows);
