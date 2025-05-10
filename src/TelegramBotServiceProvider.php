05433b9e7d9b3734001406e72896304d3aab7a88
Elyar <elyareranjbar@gmail.com>
2025-05-10 12:00:15 +0330

    Registrations

--- src/TelegramBotServiceProvider.php
+++ src/TelegramBotServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace Elyar\TelegramBotEssentials;
 
+use Elyar\TelegramBotEssentials\Console\Commands\MakeReplyKey;
+use Elyar\TelegramBotEssentials\Exceptions\LogicException;
+use Elyar\TelegramBotEssentials\Telegram\CallbackQueries\CallbackQueryBus;
+use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\ReplyKey;
+use Elyar\TelegramBotEssentials\Telegram\ReplyKeys\ReplyKeyBus;
+use Elyar\TelegramBotEssentials\Telegram\StateAnswers\StateAnswerBus;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 
 class TelegramBotServiceProvider extends ServiceProvider
@@ -10,13 +17,54 @@ class TelegramBotServiceProvider extends ServiceProvider
     {
         // Register commands here
         $this->commands([
-            \Elyar\TelegramBotEssentials\Console\Commands\MakeReplyKey::class,
+            MakeReplyKey::class,
         ]);
+
+        $this->app->singleton(ReplyKeyBus::class, function ($app) {
+            return new ReplyKeyBus();
+        });
+
+        $this->app->singleton(CallbackQueryBus::class, function ($app) {
+            return new CallbackQueryBus();
+        });
+
+        $this->app->singleton(StateAnswerBus::class, function ($app) {
+            return new StateAnswerBus();
+        });
     }
 
+    /**
+     * @throws BindingResolutionException
+     * @throws LogicException
+     */
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        // Optional: load routes, migrations, translations, etc.
+        $this->publishes([
+            __DIR__ . '/../config/telegram-bot-essentials.php' => config_path('telegram-bot-essentials.php'),
+        ], 'telegram-bot-essentials');
+
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'telegram-bot-essentials');
+        $this->publishes([
+            __DIR__.'/../lang' => resource_path('lang/vendor/telegram-bot-essentials'),
+        ], 'telegram-bot-essentials-translations');
+
+        $this->registerReplyKeys(config('telegram-bot-essentials.keyboard.admin'));
+        $this->registerReplyKeys(config('telegram-bot-essentials.keyboard.member'));
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws LogicException
+     */
+    private function registerReplyKeys(array $replyKeys): void
+    {
+        foreach ($replyKeys as $replyKeyRow) {
+            foreach ($replyKeyRow as $replyKey) {
+                if (!is_subclass_of($replyKey, ReplyKey::class))
+                    throw new LogicException("ReplyKey {$replyKey} is not a subclass of Elyar\TelegramBotEssentials\Telegram\ReplyKeys\ReplyKey");
+                replyKeyBus()->addReplyKey($replyKey);
+            }
+        }
     }
 }
