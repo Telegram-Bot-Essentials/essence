aa0e64de1976d692fa0c2df04ca657b2a5c685e1
Elyar <elyareranjbar@gmail.com>
2025-05-10 16:39:24 +0330

    Logics moved to the package

---
+++ src/Telegram/CallbackQueries/Member/MessageMetaQuery.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Elyar\TelegramBotEssentials\Telegram\CallbackQueries\Member;
+
+use Elyar\TelegramBotEssentials\Enums\Roles;
+use Elyar\TelegramBotEssentials\Exceptions\LogicException;
+use Elyar\TelegramBotEssentials\Models\MessageMeta;
+use Elyar\TelegramBotEssentials\Telegram\CallbackQueries\CallbackQuery;
+use Elyar\TelegramBotEssentials\Traits\CanCancelOldProcess;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+
+class MessageMetaQuery extends CallbackQuery
+{
+    use CanCancelOldProcess;
+
+    protected string $type = 'MESSAGE_META';
+    protected int $perm = Roles::MEMBER->value;
+
+    /**
+     * @param array $params
+     * @throws BindingResolutionException
+     * @throws LogicException
+     * @throws TelegramSDKException
+     */
+    public function handle(array $params): void
+    {
+        $this->params = $params;
+        switch (strtolower($params[0])) {
+            case "action_is_locked":
+                $this->actionIsLocked();
+                break;
+            case "cancel_action":
+                $this->cancelAction();
+                break;
+        }
+    }
+
+    /**
+     * @throws TelegramSDKException
+     */
+    private function actionIsLocked(): void
+    {
+        wHook()->api()->answerCallbackQuery([
+            'callback_query_id' => wHook()->update()->callbackQuery->id,
+            'text' => __('general.lockedForAction')
+        ]);
+    }
+
+    /**
+     * @throws TelegramSDKException
+     * @throws LogicException
+     * @throws BindingResolutionException
+     */
+    private function cancelAction(): void
+    {
+        $this->cancelOldProcess();
+        $messageMeta = MessageMeta::findOrFail($this->params[1]);
+        $messageMeta->revertAction();
+    }
+}
---
+++ src/Telegram/StateAnswers/Admin/ManageInvoiceAnswer.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Elyar\TelegramBotEssentials\Telegram\StateAnswers\Admin;
+
+use Elyar\TelegramBotEssentials\Enums\Roles;
+use Elyar\TelegramBotEssentials\Models\ToCardAttempt;
+use Elyar\TelegramBotEssentials\Telegram\StateAnswers\StateAnswer;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+
+class ManageInvoiceAnswer extends StateAnswer
+{
+    protected string $type = 'MANAGE_INVOICE';
+    protected int $perm = Roles::ADMIN->value;
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @throws TelegramSDKException
+     */
+    public function handle(string $method, array $params): void
+    {
+        $this->params = $params;
+        switch (strtolower($method)) {
+            case "reject_reason":
+                $this->rejectReason();
+                break;
+            case "cancel":
+                $this->cancel();
+                break;
+        }
+    }
+
+    private function rejectReason(): void
+    {
+        $toCardAttempt = ToCardAttempt::findOrFail($this->params['to_card_attempt_id']);
+        $toCardAttempt->rejected_at = now();
+        $toCardAttempt->reject_reason = wHook()->update()->message->text;
+        $toCardAttempt->save();
+        wHook()->user()->changeState();
+
+        wHook()->api()->sendMessage([
+            'chat_id' => wHook()->update()->message->chat->id,
+            'text' => __('invoice.to_card.text.admin-payment_rejected'),
+            'reply_to_message_id' => $toCardAttempt->messageMeta->message_id,
+            'reply_markup' => wHook()->user()->getKeyboard(),
+        ]);
+
+        $text = __('invoice.to_card.text.user-payment_rejected', [
+            'rejectionReason' => $toCardAttempt->reject_reason
+        ]);
+
+        wHook()->api()->sendMessage([
+            'chat_id' => $toCardAttempt->paymentAttempt->invoice->botUser->telegramUser->peer_id,
+            'text' => $text,
+        ]);
+
+        $toCardAttempt->messageMeta->lockAction(__('invoice.to_card.lock-keys.admin-payment_rejected_by', [
+            'adminName' => wHook()->user()->telegramUser->full_name]), customEmoji: "❌");
+        $toCardAttempt->paymentAttempt->invoice->messageMeta->lockAction(__('invoice.to_card.lock-keys.user-payment_rejected'), '❌');
+    }
+
+    /**
+     * @throws TelegramSDKException
+     */
+    function cancel(): void
+    {
+        $toCardAttempt = ToCardAttempt::findOrFail($this->params['to_card_attempt_id']);
+        $toCardAttempt->messageMeta->revertAction();
+    }
+}
