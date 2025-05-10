ef5c2a274e16ca98f81c1513208228214f658676
Elyar <elyareranjbar@gmail.com>
2025-05-10 12:59:59 +0330

    Bot Settings feature

---
+++ lang/en/bot_settings.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'settingsText' => 'Settings'
+        . "\r\n"
+        . "\r\nBot Status :botStatus"
+        . "\r\nPay With Card :payWithCardStatus"
+        . "\r\n"
+        . "\r\nPayments Card Number: :paymentCardNumber"
+        . "\r\nPayments Card Name: :paymentCardName"
+        . "\r\n"
+        . "\r\nTransactions chat ID: :transactionsChatId",
+
+    // inline keys
+    'botStatusSwitch' => 'Bot Status',
+    'botIsOffAlertMessage' => 'Bot is currently out of service.',
+    'payWithCardSwitch' => 'Pay with Card',
+
+    'keysSeparatorPlaceHolder' => '===============',
+
+    'paymentCardNumber' => 'Change Payment Card Number',
+    'changePaymentCardNumberMessage' => 'Enter new payment card number: ',
+
+    'paymentCardName' => 'Change Payment Card Name',
+    'changePaymentCardNameMessage' => 'Enter new payment card name: ',
+
+    'transactionsChat' => 'Change Transactions Chat ID',
+    'transactionsChatIdMessage' => 'Enter new transactions chat ID: ',
+
+    'reply_key' => 'Bot Settings',
+];
