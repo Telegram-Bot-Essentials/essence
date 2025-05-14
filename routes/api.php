22775722680fbb109d3127c1b732242c164e6507
Elyar <elyareranjbar@gmail.com>
2025-05-14 11:01:06 +0330

    Answering get requests with 200

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,8 @@ use Elyar\TelegramBotEssentials\Http\Controllers\TelegramWebhookController;
 use Elyar\TelegramBotEssentials\Http\Middleware\TelegramBotAuthentication;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/telegram/bot/{unique_id}/webhook', TelegramWebhookController::class)->middleware(TelegramBotAuthentication::class);
+Route::get('/telegram/bot/{unique_id}/webhook', function (){
+    return response('OK',200);
+});
+
+Route::post('/telegram/bot/{unique_id}/webhook', TelegramWebhookController::class)->middleware(TelegramBotAuthentication::class);
