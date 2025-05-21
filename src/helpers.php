d31ae5eebdb08ae27c01b1c6d0f9e7f886847b72
Elyar <elyareranjbar@gmail.com>
2025-05-21 14:04:26 +0330

    Returning price as raw

--- src/helpers.php
+++ src/helpers.php
@@ -201,11 +201,11 @@ if (!function_exists('getNextFromArray')) {
 }
 
 if (!function_exists('priceFormat')) {
-    function priceFormat(float $price): string
+    function priceFormat(float $price, bool $raw = false): string
     {
         $symbol = getDefaultCurrencySymbol();
         $seperator = preg_match('/[\x{0600}-\x{06FF}\x{0750}-\x{077F}\x{08A0}-\x{08FF}\x{FB50}-\x{FDFF}\x{FE70}-\x{FEFF}\x{FDFC}]/u', $symbol) ? ' ' : '';
-        return formatFloat($price) . $seperator . $symbol;
+        return $raw ? formatFloat($price) . $seperator . $symbol : $price . $seperator . $symbol;
     }
 }
 
