0b32e82ec8a4e17e0a1968d005ae3dcbc77d5ee5
Elyar <elyareranjbar@gmail.com>
2025-05-26 13:16:31 +0330

    No need for extra organization

--- tests/Pest.php
+++ tests/Pest.php
@@ -18,8 +18,6 @@ pest()->extend(Elyar\TelegramBotEssentials\Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
-pest()->extend(Elyar\TelegramBotEssentials\Tests\TenantTestCase::class)->in('Tenant');
-
 /*
 |--------------------------------------------------------------------------
 | Expectations
