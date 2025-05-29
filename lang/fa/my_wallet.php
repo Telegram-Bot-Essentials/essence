bf812be429b013bcd6cb352266203166f8073138
Elyar <elyar.ranjbar0@gmail.com>
2025-05-29 19:04:43 +0330

    Translation strings for my wallet

---
+++ lang/fa/my_wallet.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'main' => [
+        'text' => [
+            'totalCredit' => "مجموع اعتبار: :price",
+            'enterCreditAmount' => "مقدار اعتباری که می‌خواهید به حساب خود اضافه کنید را وارد کنید:",
+        ],
+        'answers' => [
+        ],
+        'keys' => [
+            'addCredit' => 'افزایش اعتبار 💲',
+        ],
+    ],
+    'reply_key' => 'کیف پول من 💰',
+];
