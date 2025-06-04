7f52bbaa56985ddb8e89148f60f2004df973970c
Elyar <elyar.ranjbar0@gmail.com>
2025-06-04 16:24:34 +0330

    Bug fix

--- src/Jobs/InvoicePaidHookJob.php
+++ src/Jobs/InvoicePaidHookJob.php
@@ -45,10 +45,6 @@ class InvoicePaidHookJob implements ShouldQueue
 
         \App::setLocale(wHook()->bot()->settings->language);
 
-        if($this->invoice->status == 'paid') {
-            return;
-        }
-
         try {
             wHook()->api()->sendMessage([
                 'chat_id' => $this->invoice->botUser->telegramUser->peer_id,
