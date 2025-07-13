4d155e8a9accee25bbc7e311e22896f0f76c5d74
Elyar <elyareranjbar@gmail.com>
2025-07-13 19:54:43 +0330

    Improved set webhook command

--- src/Console/Commands/setWebhook.php
+++ src/Console/Commands/setWebhook.php
@@ -30,26 +30,28 @@ class setWebhook extends Command
      */
     public function handle()
     {
-        $uniqueID = $this->option('unique-id');
-        if ($uniqueID) {
-            $this->info('Setting webhook for bot with unique id: ' . $uniqueID);
-            $this->info('Telegram bot api url: ' . config('telegram-bot-essentials.base_bot_url'));
+        $uniqueID = $this->option('unique-id') ?? "main";
+        $this->info('Setting webhook for bot with unique id: ' . $uniqueID);
+        $this->info('Telegram bot api url: ' . config('telegram-bot-essentials.base_bot_url'));
 
-            $url = config('app.url') . "/api/{$uniqueID}/telegram/bot/webhook";
-            $this->info('Webhook url: ' . $url);
-            $bot = Bot::where('unique_id', $uniqueID)->firstOrFail();
-            $telegram = new Api(
-                token: $bot->bot_token,
-                baseBotUrl: config('telegram-bot-essentials.base_bot_url')
-            );
-            $telegram->setWebhook([
-                'url' => $url,
-                'drop_pending_updates' => true,
-                'secret_token' => $bot->secret_token,
-            ]);
-            $this->info('Telegram webhook is set');
-        } else {
-            $this->error('Unique id is not provided');
+        $url = config('app.url') . "/api/{$uniqueID}/telegram/bot/webhook";
+        $this->info('Webhook url: ' . $url);
+        $bot = Bot::where('unique_id', $uniqueID)->first();
+
+        if(!$bot){
+            $this->error('Bot with unique id: ' . $uniqueID . ' not found');
+            return;
         }
+
+        $telegram = new Api(
+            token: $bot->bot_token,
+            baseBotUrl: config('telegram-bot-essentials.base_bot_url')
+        );
+        $telegram->setWebhook([
+            'url' => $url,
+            'drop_pending_updates' => true,
+            'secret_token' => $bot->secret_token,
+        ]);
+        $this->info('Telegram webhook has been set');
     }
 }
