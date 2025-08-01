1ae5522410a4c990fa3f08a5164fa1bbf6fa8797
Elyar <elyareranjbar@gmail.com>
2025-08-01 11:16:00 +0330

    Improved ExceptionHandler

--- src/Services/ExceptionHandler.php
+++ src/Services/ExceptionHandler.php
@@ -53,6 +53,7 @@ class ExceptionHandler
                 Log::error($e->getMessage());
             }
             exceptionReport($e);
+            abort(200, 'Something went wrong');
         }
     }
 
