66ca57b5d08f312c032deb2e5995a7f4a68b0958
Elyar <elyareranjbar@gmail.com>
2025-09-22 22:31:35 +0330

    Revert "Testing"
    This reverts commit 0cba44f22c13ac2733cab8adce4d34773e7daff0.

--- src/Http/Controllers/TelegramWebhookController.php
+++ src/Http/Controllers/TelegramWebhookController.php
@@ -29,136 +29,16 @@ class TelegramWebhookController extends Controller
         try {
             dependsOn(!wHook()->bot()->suspended && (is_null(wHook()->bot()->activated_until) || wHook()->bot()->activated_until->isFuture()), __('tbe::general.alerts.botIsOff'));
             if (!hasAccess()) dependsOn(wHook()->bot()->settings->bot_status, __('tbe::general.alerts.botIsOff'));
-            $this->initializeOptions();
+            $commands = config('tbe-essence.commands') ?? [];
+
+            foreach ($commands as $command) {
+                if (!is_subclass_of($command, Command::class))
+                    throw new LogicException("ReplyKey {$command} is not a subclass of namespace Telegram\Bot\Commands\Command");
+                wHook()->api()->addCommand($command);
+            }
             $this->processUpdate();
         } catch (Exception $e) {
             exceptionHandler()->handle($e);
-        }
-    }
-
-    /**
-     * @throws LogicException
-     * @throws TelegramSDKException
-     * @throws BindingResolutionException
-     */
-    private function initializeOptions()
-    {
-        $commands = config('tbe-essence.commands') ?? [];
-
-        foreach ($commands as $command) {
-            if (!is_subclass_of($command, Command::class))
-                throw new LogicException("ReplyKey {$command} is not a subclass of namespace Telegram\Bot\Commands\Command");
-            wHook()->api()->addCommand($command);
-        }
-
-        $adminQueries = base_path('app/Telegram/CallbackQueries/Admin');
-        $memberQueries = base_path('app/Telegram/CallbackQueries/Member');
-        $adminStateAnswers = base_path('app/Telegram/StateAnswers/Admin');
-        $memberStateAnswers = base_path('app/Telegram/StateAnswers/Member');
-//        $adminReplyKeys = base_path('app/Telegram/ReplyKeys/Admin');
-//        $memberReplyKeys = base_path('app/Telegram/ReplyKeys/Member');
-        if (is_dir($adminQueries)) $this->autoLoadCallbackQueries($adminQueries);
-        if (is_dir($memberQueries)) $this->autoLoadCallbackQueries($memberQueries);
-        $this->autoLoadCallbackQueries(realpath(__DIR__ . '/../../Telegram/CallbackQueries/Member'));
-        $this->autoLoadCallbackQueries(realpath(__DIR__ . '/../../Telegram/CallbackQueries/Admin'));
-
-        if (is_dir($adminStateAnswers)) $this->autoLoadStateAnswers($adminStateAnswers);
-        if (is_dir($memberStateAnswers)) $this->autoLoadStateAnswers($memberStateAnswers);
-        $this->autoLoadStateAnswers(realpath(__DIR__ . '/../../Telegram/StateAnswers/Member'));
-        $this->autoLoadStateAnswers(realpath(__DIR__ . '/../../Telegram/StateAnswers/Admin'));
-
-        $this->addUserReplyKeys(config('telegram-bot-essentials.keyboard.admin') ?? []);
-        $this->addUserReplyKeys(config('telegram-bot-essentials.keyboard.member') ?? []);
-        $this->autoLoadReplyKeys(realpath(__DIR__ . '/../../Telegram/ReplyKeys/Member'));
-        $this->autoLoadReplyKeys(realpath(__DIR__ . '/../../Telegram/ReplyKeys/Admin'));
-    }
-
-    /**
-     * @param string $path
-     * @return void
-     * @throws BindingResolutionException
-     * @throws LogicException
-     */
-    private function autoLoadCallbackQueries(string $path): void
-    {
-        $namespace = $this->resolveNamespace($path);
-
-        foreach (File::allFiles($path) as $file) {
-            $fqcn = $namespace . '\\' . $file->getFilenameWithoutExtension();
-
-            if (class_exists($fqcn) && is_subclass_of($fqcn, CallbackQuery::class)) {
-                callbackQueryBus()->addCallbackQuery($fqcn);
-            }
-        }
-    }
-
-    /**
-     * @param string $path
-     * @return string
-     */
-    private function resolveNamespace(string $path): string
-    {
-        if (str_starts_with($path, base_path('app'))) {
-            $basePath = base_path('app');
-            $baseNamespace = app()->getNamespace();
-        } else {
-            $basePath = realpath(__DIR__ . '/../../');
-            $baseNamespace = 'TelegramBotEssentials\\Essence';
-        }
-
-        $relativePath = str_replace($basePath . DIRECTORY_SEPARATOR, '', $path);
-        $relativeNamespace = str_replace('/', '\\', $relativePath);
-
-        return trim(rtrim($baseNamespace, '\\') . '\\' . $relativeNamespace, '\\');
-    }
-
-    /**
-     * @param string $path
-     * @throws BindingResolutionException
-     * @throws LogicException
-     */
-    private function autoLoadStateAnswers(string $path): void
-    {
-        $namespace = $this->resolveNamespace($path);
-
-        foreach (File::allFiles($path) as $file) {
-            $fqcn = $namespace . '\\' . $file->getFilenameWithoutExtension();
-
-            if (class_exists($fqcn) && is_subclass_of($fqcn, StateAnswer::class)) {
-                stateAnswerBus()->addStateAnswer($fqcn);
-            }
-        }
-    }
-
-    /**
-     * @throws BindingResolutionException
-     * @throws LogicException
-     */
-    private function addUserReplyKeys(array $replyKeys): void
-    {
-        foreach ($replyKeys as $replyKeyRow) {
-            foreach ($replyKeyRow as $replyKey) {
-                if (!is_subclass_of($replyKey, ReplyKey::class))
-                    throw new LogicException("ReplyKey {$replyKey} is not a subclass of TelegramBotEssentials\Essence\Telegram\ReplyKeys\ReplyKey");
-                replyKeyBus()->addReplyKey($replyKey);
-            }
-        }
-    }
-
-    /**
-     * @throws BindingResolutionException
-     * @throws LogicException
-     */
-    private function autoLoadReplyKeys(string $path): void
-    {
-        $namespace = $this->resolveNamespace($path);
-
-        foreach (File::allFiles($path) as $file) {
-            $fqcn = $namespace . '\\' . $file->getFilenameWithoutExtension();
-
-            if (class_exists($fqcn) && is_subclass_of($fqcn, ReplyKey::class)) {
-                replyKeyBus()->addReplyKey($fqcn);
-            }
         }
     }
 
