9293f3a872cd18b1c5c74604d749a0dcb30ece0a
Elyar <elyareranjbar@gmail.com>
2025-09-26 20:42:19 +0330

    peerId resolving function

--- src/Support/Webhook.php
+++ src/Support/Webhook.php
@@ -3,11 +3,11 @@
 namespace TelegramBotEssentials\Essence\Support;
 
 use Closure;
+use Telegram\Bot\Api;
+use Telegram\Bot\Objects\Update;
 use TelegramBotEssentials\Essence\Exceptions\WebhookAuthException;
 use TelegramBotEssentials\Essence\Models\Bot;
 use TelegramBotEssentials\Essence\Models\BotUser;
-use Telegram\Bot\Api;
-use Telegram\Bot\Objects\Update;
 
 class Webhook
 {
@@ -25,17 +25,6 @@ class Webhook
     public static function setUpdate(?Update $update): void
     {
         self::$update = $update;
-    }
-
-    public static function setBot(?Bot $bot): void
-    {
-        self::$bot = $bot;
-    }
-
-    public static function setUser(?BotUser $user): void
-    {
-        self::$user = $user;
-        self::$requestState = $user->state;
     }
 
     public static function api(): Api
@@ -110,5 +99,37 @@ class Webhook
             self::setBot($originalBot);
             self::setUser($originalUser);
         }
+    }
+
+    public static function setBot(?Bot $bot): void
+    {
+        self::$bot = $bot;
+    }
+
+    public static function setUser(?BotUser $user): void
+    {
+        self::$user = $user;
+        self::$requestState = $user->state;
+    }
+
+    public static function peerId(): int
+    {
+        if (self::$update->isType('callback_query')) {
+            return self::$update->callbackQuery->from->id;
+        }
+
+        if (self::$update->isType('message')) {
+            return self::$update->message->from->id;
+        }
+
+        if (self::$update->isType('inline_query')) {
+            return self::$update->inlineQuery->from->id;
+        }
+
+        if (isset(self::$update->chat)) {
+            return self::$update->chat->id;
+        }
+
+        throw new WebhookAuthException('Failed to retrieve telegram peer id.');
     }
 }
--- src/Telegram/CallbackQueries/CallbackQueryInterface.php
+++ src/Telegram/CallbackQueries/CallbackQueryInterface.php
@@ -7,6 +7,6 @@ interface CallbackQueryInterface
     public function getType(): string;
 
     public function getPerm(): int;
-
+    public function setParams(?array $params): void;
     public function handle(): void;
 }
